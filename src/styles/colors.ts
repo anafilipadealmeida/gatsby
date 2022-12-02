d45ccdf5f1aa831d39a24145e5a60bcfa8ba4ce7
Ana Almeida <anafilipadealmeida@gmail.com>
2022-12-02 00:42:35 +0000

    Fix color strings

--- src/styles/colors.ts
+++ src/styles/colors.ts
@@ -1,7 +1,7 @@
 module.exports = {
-  black: "000000",
-  blue: "e1f4f3",
-  grayDark: "333333",
-  grayMedium: "706c61",
-  white: "ffffff",
+  black: "#000000",
+  blue: "#e1f4f3",
+  grayDark: "#333333",
+  grayMedium: "#706c61",
+  white: "#ffffff",
 }
