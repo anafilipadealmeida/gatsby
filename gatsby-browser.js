a183cce68f77895438117d807fff5c6286980c67
Ana Almeida <anafilipadealmeida@gmail.com>
2022-12-05 20:14:23 +0000

    Fix fontawesome rending flash

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -4,4 +4,7 @@
  * See: https://www.gatsbyjs.com/docs/reference/config-files/gatsby-browser/
  */
 
-// You can delete this file if you're not using it
+const styles = require("@fortawesome/fontawesome-svg-core/styles.css")
+const config = require("@fortawesome/fontawesome-svg-core")
+
+config.autoAddCss = false
